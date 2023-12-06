5b5b19a25d83ffa7e84aa4f41d7da6c9a4363e3f
Abdelhamied <abdelhamied.amr@outlook.com>
2023-12-07 00:52:19 +0200

    init commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+import uvicorn
+
+app = FastAPI()
+
+@app.get('/', response_class=HTMLResponse)
+def html_response():
+    return '<h1> Hello World:)! </h1>'
+
+@app.get('/hello/{user}', response_class=HTMLResponse)
+def hello_user(user:str):
+    return f'<h1>Hello {user}</h1>'
+
+@app.get('/bye/{user}', response_class=HTMLResponse)
+def byes_user(user:str):
+    return f'<h1>Bye {user}</h1>'
+
